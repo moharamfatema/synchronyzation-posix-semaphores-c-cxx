aac2ac8ff5e579b5f234ca33b76dd20f441d76da
Fatema Moharam <5445035-moharamfatema@users.noreply.gitlab.com>
2021-12-16 14:55:17 +0200

    +counter threads creator

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 #include <semaphore.h>
 #include <fstream>
 
-#define MAX_SLEEP_TIME 1000
+#define MAX_SLEEP_TIME 20
+#define MAX_RUN_TIME 100
 
 /*assume the longest runtime is 1 second for now*/
 /*we need a timer class*/
@@ -17,32 +18,40 @@ class Timer
 
     std::chrono::time_point<std::chrono::system_clock> start;
     std::chrono::duration<float> duration;
-    public:
-    Timer(){
-         start = std::chrono::high_resolution_clock::now();
+
+public:
+    Timer()
+    {
+        start = std::chrono::high_resolution_clock::now();
     }
-    
-    float getTimeElapsed(){
-        auto end  = std::chrono::high_resolution_clock::now();
+
+    float getTimeElapsed()
+    {
+        auto end = std::chrono::high_resolution_clock::now();
         duration = end - start;
-        
-        return duration.count()*1000.00f;
+
+        return duration.count() * 1000.00f;
     }
 };
 
-struct counterArgs{
-    sem_t semaphore;
-    unsigned int counter;
-    std::ofstream * fout;
+/*the parameters passed to a new counter thread*/
+struct counterArgs
+{
+    sem_t *semaphore;
+    unsigned int *counter;
+    std::ofstream *fout;
     unsigned int threadNumber;
-    
-    public:
-    counterArgs(
-        sem_t semaphore,
-        unsigned int counter,
-        std::ofstream * fout,
-        const unsigned int threadNumber
-        ){
+
+public:
+    counterArgs()
+    {
+    }
+    explicit counterArgs(
+        sem_t *semaphore,
+        unsigned int *counter,
+        std::ofstream *fout,
+        const unsigned int threadNumber)
+    {
         this->counter = counter;
         this->fout = fout;
         this->semaphore = semaphore;
@@ -50,45 +59,76 @@ struct counterArgs{
     }
 };
 
-
-void counter(void * argsptr){
+/*fuction to be passed into pthread create for a new counter thread*/
+void *mCounter(void *argsptr)
+{
     /*extracting arguments*/
-    counterArgs * args = (counterArgs *) argsptr;
-    
-    
-    /*go to sleep*/
-    srandom((int)std::chrono::high_resolution_clock::now().time_since_epoch().count());
-    usleep(random()%MAX_SLEEP_TIME);
-    
-    /*write to file*/
-    *args->fout 
-        << "Counter Thread "
-        <<args->threadNumber
-        <<" : recieved a message.\n";
+    counterArgs *args = (counterArgs *)argsptr;
+    unsigned int currentCounterValue;
+    std::string msg = "";
+    while (1)
+    {
+        /*go to sleep*/
+        srandom((int)std::chrono::high_resolution_clock::now().time_since_epoch().count());
+        usleep(random() % MAX_SLEEP_TIME);
 
-    
-    *args->fout 
-        << "Counter Thread "
-        <<args->threadNumber
-        <<" : waiting to write.\n";
+        /*write to file*/
+        msg = "";
+        msg = msg.append(
+            "Counter Thread " + std::to_string(args->threadNumber) + " : recieved a message.\n");
+        *args->fout << msg;
+        msg = "";
 
-    sem_wait(&args->semaphore);
-    /*critical section*/{
-        /*write to counter args.counter*/
-        args->counter++;
-        /*write to file args.fin current value of counter*/
-        *args->fout 
-            << "Counter Thread "
-            << args->threadNumber
-            <<" : now adding to counter,\n"
-            <<"counter value = "
-            <<args->counter
-            <<"\n";
+        msg = msg.append(
+            "Counter Thread " + std::to_string(args->threadNumber) + " : waiting to write.\n");
+        *args->fout << msg;
+        msg = "";
+
+        sem_wait(args->semaphore);
+        /*critical section*/ {
+            /*write to counter args.counter and*/
+            currentCounterValue = ++(*args->counter);
+            /*write to file args.fin current value of counter*/
+            msg = msg.append(
+                "Counter Thread " + std::to_string(args->threadNumber) + " : now adding to counter,\n" + "counter value = " + std::to_string(currentCounterValue) + "\n");
+            *args->fout << msg;
+        }
+
+        sem_post(args->semaphore);
+    }
+}
+
+/*create n mCounter threads*/
+int createCounters(sem_t *semaphore, unsigned int *counter, const unsigned int n, std::ofstream *fout)
+{
+
+    pthread_t tid[n];
+
+    counterArgs counterArgsArray[n];
+
+    Timer timer;
+
+    for (int i = 0; i < n; i++)
+    {
+        /*setting the parameters*/
+        counterArgsArray[i].counter = counter;
+        counterArgsArray[i].fout = fout;
+        counterArgsArray[i].semaphore = semaphore;
+        counterArgsArray[i].threadNumber = i;
     }
 
-    /*end critical section*/
-    sem_post(&args->semaphore);
+    for (int j = 0; j < n; j++)
+    {
 
+        pthread_create(&tid[j], nullptr, mCounter, &counterArgsArray[j]);
+    }
 
+    
+    while(timer.getTimeElapsed() < MAX_RUN_TIME);
+    *fout << "\n---Exiting---";
+    /*(*fout).close();
+    */
 
+    return 0;
 }
+
--- test.cpp
+++ test.cpp
@@ -9,7 +9,7 @@
     EXPECT_NEAR(time,5,0.1);
 }*/
 
-TEST(TestCounters,TestCounter){
+/*TEST(TestCounters,TestCounter){
     const std::string outMessage = 
     "Counter Thread 1 : recieved a message. Counter Thread 1 : waiting to write. Counter Thread 1 : now adding to counter, counter value = 1  ";
 
@@ -21,10 +21,13 @@ TEST(TestCounters,TestCounter){
     std::ofstream fout;
     fout.open("out.txt");
     unsigned int count = 0;
-    unsigned int threadNumber = 1;
+    const unsigned int threadNumber = 1;
     counterArgs args = counterArgs(semaphore,count,&fout,threadNumber);
-    counter((void *)&args);
+    mCounter((void *)&args);
     fout.close();
+
+    exit(0);
+
     fin.open("out.txt");
 
     fileOut = "";
@@ -33,6 +36,25 @@ TEST(TestCounters,TestCounter){
         fileOut = fileOut.append(str + " ");
     }
     EXPECT_EQ(fileOut,outMessage);
+}*/
+
+TEST(TestCounters,TestCreateCounters){
+    
+    sem_t semaphore;
+    sem_init(&semaphore,0,1);
+    std::ofstream fout;
+    fout.open("out.txt");
+    unsigned int * count = new unsigned int;
+    *count  = 0;
+    
+    Timer timer;
+    if(fork() == 0 ){
+        createCounters(&semaphore,count,5,&fout);
+        return;
+    }
+    wait(nullptr);
+    fout.close();
+    EXPECT_NEAR(timer.getTimeElapsed(),MAX_RUN_TIME,MAX_RUN_TIME/10);
 }
 
 int main(int argc, char **argv) {
